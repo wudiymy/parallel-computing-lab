369ae0dc6ecb917b9f68d5efb9651973c8ec7e82
gpzlx1 <gpzlx1@mail.ustc.edu.cn>
2019-04-15 05:25:09 -0700

    pi_serial

---
+++ omp/pi_serial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+#define steps 100000000
+
+void main(){
+	double x, pi, sum = 0;
+	double step = 1.0 / steps;
+	for(int i = 1; i < steps; i++){
+		x = (i - 0.5) * step;
+		sum = sum + 4.0/(1.0 + x * x);
+	}
+	pi = step * sum;
+	printf("\npi=%0.20lf\n", pi);
+}
+
